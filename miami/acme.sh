e375fb9722b02e2f173b6faae4d09d18cc857b09
jason long <73072111+jlongua@users.noreply.github.com>
2021-02-17 09:29:27 -0500

    namesilo

--- miami/acme.sh
+++ miami/acme.sh
@@ -4,4 +4,6 @@ apt install socat
 ok vultr has rolled out a new api v2 so no more dns challenge for certs...
 but acme,sh saves the day again with alias mode!
 Using a provider with a supported api I can issue certs for my vultr hosts.
+Looks like I can register a domain with namesilo and user their api with acme.sh to manipulate the dns records.
+Hopefully they don't have any limits using their api like namecheap.
 
