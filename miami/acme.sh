ca4ba9dd4fe0146e2828ec1a29c0a06e8de95bfd
jason long <73072111+jlongua@users.noreply.github.com>
2021-02-16 22:41:07 -0500

    acme.sh alias mode

--- miami/acme.sh
+++ miami/acme.sh
@@ -1,3 +1,7 @@
 ```sh
 apt install socat
 ```
+ok vultr has rolled out a new api v2 so no more dns challenge for certs...
+but acme,sh daves the day again with alias mode!
+Using a provider with a supported api I can issue certs for my vultr hosts.
+
