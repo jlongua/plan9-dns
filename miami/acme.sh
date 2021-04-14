7bd83356ec52c9680129d31f9c6fcda160db059e
jason long <73072111+jlongua@users.noreply.github.com>
2021-04-14 17:40:45 -0400

    lexicon updated for vultr API v2

--- miami/acme.sh
+++ miami/acme.sh
@@ -1,8 +1,5 @@
 ```sh
 apt install socat
 ```
-ok, vultr has rolled out a new api v2 so no more dns challenge for certs...
-but I can use acme.sh with alias mode and a supported domain registrar.
-I have registered a domain with namesilo and have succesfuly used their api with acme.sh to manipulate the dns records.
-So looking for someone with python skills to redo the acme.sh or lexicon to work with vultr v2 api.
-I put in a pull request for lexicon and looks like I was elected! Unfortunately I can barely write bash scripts...
+- acme.sh dns using lexicon has been updated to work with vultr API v2
+- at the moment still using acme.sh with alias mode and namesilo registrar.
