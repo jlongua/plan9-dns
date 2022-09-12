555d37c3aa33b2476f0cbe46dc007f90a3808d1c
jason long <73072111+jlongua@users.noreply.github.com>
2022-09-12 11:43:12 -0400

    Create certsPush.sh

---
+++ testing/certsPush.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# stat -c %Z fullchain.pem > last
+# last=$(stat -c %Z fullchain.pem)
+
+# first time init
+#
+if [[ -f "/etc/zerossl/plan9-dns.com_ecc/last" ]]; then
+    echo "File is exist"
+else
+   echo "File is not exist"
+   echo "creating file last"
+   stat -c %Z /etc/zerossl/plan9-dns.com_ecc/fullchain.pem > /etc/zerossl/plan9-dns.com_ecc/last
+fi
+
+# check for updated SSl Certs
+#
+if [[ $(cat /etc/zerossl/plan9-dns.com_ecc/last) != $(stat -c %Z /etc/zerossl/plan9-dns.com_ecc/fullchain.pem) ]]; then
+    echo "fullchain.pem has changed"
+    echo "scp new certs to vps's"
+    chmod 640 /etc/zerossl/plan9-dns.com_ecc/private.key
+    sudo -u  yourUser /usr/bin/scp /etc/zerossl/plan9-dns.com_ecc/fullchain.pem helios-ssl:/home/yourUser/ssl
+    sudo -u  yourUser /usr/bin/scp /etc/zerossl/plan9-dns.com_ecc/private.key helios-ssl:/home/yourUser/ssl
+    chmod 600 /etc/zerossl/plan9-dns.com_ecc/private.key
+    stat -c %Z /etc/zerossl/plan9-dns.com_ecc/fullchain.pem > /etc/zerossl/plan9-dns.com_ecc/last
+else
+    echo "fullchain.pem has NOT changed"
+fi
