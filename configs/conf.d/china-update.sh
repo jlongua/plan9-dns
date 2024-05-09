fddb801725c9fd47c9b87b38306b75754ebe23a8
jason long <73072111+jlongua@users.noreply.github.com>
2024-05-09 04:22:04 +0000

    minor comment corrections

--- configs/conf.d/china-update.sh
+++ configs/conf.d/china-update.sh
@@ -4,12 +4,12 @@ wget -w 1 -O china_cidr https://raw.githubusercontent.com/ipverse/rir-ip/master/
 
 sleep 2
 
-sed -i 'N;$!P;D' china_cidr  # delete nexr to last line
+sed -i 'N;$!P;D' china_cidr  # delete next to last line
 sed -i '1,8d' china_cidr # delete lines 1-8
 sed -i 's/\[/\{/g' china_cidr # subsitute { for [
 sed -i 's/\]/\}/g' china_cidr # substitue } for ]
 sed -i 's/"ipv4":/ipv4 =/g' china_cidr # substitute ipv4 = for "ipv4"
-sed -i 's/"ipv6":/ipv6 =/g' china_cidr # substitute ipv4 = for "ipv6"
+sed -i 's/"ipv6":/ipv6 =/g' china_cidr # substitute ipv6 = for "ipv6"
 sed -i 's/  "subnets":/local china =/g' china_cidr # substitute local china = for "subnets":
 sed -i -e '$a\' china_cidr # add newline at end of file
 
